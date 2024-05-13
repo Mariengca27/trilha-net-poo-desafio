1155b7e8c993017152b2fb32fff0b6b56d40f8f0
Mariana Machado <marimeltywork@outlook.com>
2024-05-12 23:31:28 -0300

    Instancias das classes Nokie e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Iphone12");
+Iphone iphone = new Iphone("123456789", "Iphone 12", "123456789", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Testando Nokia3310");
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "987654321", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+
